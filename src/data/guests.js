745bde7508f95a4662caee61369f4ec7f01d4d89
Viviana9110 <v.londono.naranjo@gmail.com>
2026-07-03 16:01:25 -0500

    actualizacion invitados

--- src/data/guests.js
+++ src/data/guests.js
@@ -1,7 +1,8 @@
 const guests = [
   { name: "Viviana Londoño", phone: "573137622767" },
   { name: "Andres Zapata", phone: "573205394993" },
-  { name: "Martha", phone: "573127971334"}
+  { name: "Martha", phone: "573127971334"},
+  { name: "Felipe Rios de Grisales", phone: "573135931483"}
 ];
 
 export default guests;
